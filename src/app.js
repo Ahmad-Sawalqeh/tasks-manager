7bc7f78024b9bf607fb990cf5017186431e4afd3
Ahmad-Sawalqeh <53290785+Ahmad-Sawalqeh@users.noreply.github.com>
2020-04-08 22:03:24 +0300

    small fix

--- src/app.js
+++ src/app.js
@@ -68,3 +68,78 @@ class App extends Component {
 }
 
 export default App;
+
+/*
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      userInput : '',
+      list : []
+    }
+
+    this.changeUserInput = this.changeUserInput.bind(this);
+    this.addToList = this.addToList.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+
+  changeUserInput(e){
+    let input = e.target.value;
+    this.setState({
+      userInput : input
+    });
+  }
+
+  addToList(){
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.userInput
+    }
+    let newList = [...this.state.list];
+    newList.push(newItem);
+    this.setState({
+      userInput : '',
+      list : newList
+    });
+  }
+
+  deleteItem(id){
+    const list = [...this.state.list];
+    const updateList = list.filter(val => val.id !== id);
+    this.setState({
+      list: updateList
+    });
+  }
+
+  render(){
+    return (
+      <>
+        <h1 className='app-title'>My List</h1>
+        <div className='container'>
+          <div className='to-do'>
+            Add an Item ...
+            <br />          
+            <input className='inputField' onChange={this.changeUserInput} value={this.state.userInput} type='text' placeholder='type item here ...' />
+            <button className='btn add-btn' onClick={this.addToList} >Add Item</button>
+            <ul>
+              {
+                this.state.list.map((val, idx) =>{
+                  return(
+                    <>
+                      <li className='list' key={val.id}>{idx + 1}- {val.value}</li>
+                      <button className='btn dele-btn' onClick={this.deleteItem(val.id)} >X</button>
+                    </>
+                    );
+                  }
+                )
+              }
+            </ul>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
+*/
