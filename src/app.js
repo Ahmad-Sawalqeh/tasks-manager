080e223c26a83c3402c686f1e434cc28791783d1
Ahmad-Sawalqeh <53290785+Ahmad-Sawalqeh@users.noreply.github.com>
2020-04-09 14:35:45 +0300

    adding counter app

--- src/app.js
+++ src/app.js
@@ -1,145 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './app.css';
 
-class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      userInput : '',
-      list : []
-    }
-  }
+// state is Mutable (changable)
+// props is Immutable (read only)
 
-  changeUserInput(input){
-    this.setState({
-      userInput : input
-    });
-  }
+function Button(props) {
+  const handleClick = () => props.onClickFunction(props.increment);
+	return (
+  	<button className='btn add-btn' onClick={handleClick}>
+      +{props.increment}
+    </button>
+  );
+}
 
-  addToList(input){
-    const newItem = {
-      id: 1 + Math.random(),
-      value: input
-    }
-    let list = [...this.state.list];
-    list.push(newItem);
-    this.setState({
-      userInput : '',
-      list
-    });
-  }
+function Display(props) {
+	return (
+  	<div>{props.message}</div>
+  );
+}
 
-  deleteItem(id){
-    const list = [...this.state.list];
-    const updateList = list.filter(val => val.id !== id);
-    this.setState({
-      list: updateList
-    });
-  }
-
-  render(){
-    return (
-      <>
-        <h1 className='app-title'>My List</h1>
-        <div className='container'>
-          <div className='to-do'>
-            Add an Item ...
-            <br />          
-            <input className='inputField' onChange={e=>this.changeUserInput(e.target.value)} value={this.state.userInput} type='text' placeholder='type item here ...' />
-            <button className='btn add-btn' onClick={()=>this.addToList(this.state.userInput)} >Add Item</button>
-            <ul>
-              {
-                this.state.list.map((val, idx) =>{
-                  return(
-                    <>
-                      <li className='list' key={val.id}>{idx + 1}- {val.value}</li>
-                      <button className='btn dele-btn' onClick={()=>this.deleteItem(val.id)} >X</button>
-                    </>
-                    );
-                  }
-                )
-              }
-            </ul>
-          </div>
+function App() {
+	const [counter, setCounter] = useState(0);
+  const incrementCounter = (incrementValue) => setCounter(counter+incrementValue);
+	return (
+    <>
+      <h1 className='app-title'>My Counter</h1>
+      <div className='container'>
+        <div className='to-do'>
+          <Button onClickFunction={incrementCounter} increment={1} />
+          <Button onClickFunction={incrementCounter} increment={5} />
+          <Button onClickFunction={incrementCounter} increment={10} />
+          <Button onClickFunction={incrementCounter} increment={100} />
+          <Display message={counter}/>
         </div>
-      </>
-    );
-  }
+      </div>  
+    </>
+  );
 }
 
 export default App;
-
-/*
-class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      userInput : '',
-      list : []
-    }
-
-    this.changeUserInput = this.changeUserInput.bind(this);
-    this.addToList = this.addToList.bind(this);
-    this.deleteItem = this.deleteItem.bind(this);
-  }
-
-  changeUserInput(e){
-    let input = e.target.value;
-    this.setState({
-      userInput : input
-    });
-  }
-
-  addToList(){
-    const newItem = {
-      id: 1 + Math.random(),
-      value: this.state.userInput
-    }
-    let newList = [...this.state.list];
-    newList.push(newItem);
-    this.setState({
-      userInput : '',
-      list : newList
-    });
-  }
-
-  deleteItem(id){
-    const list = [...this.state.list];
-    const updateList = list.filter(val => val.id !== id);
-    this.setState({
-      list: updateList
-    });
-  }
-
-  render(){
-    return (
-      <>
-        <h1 className='app-title'>My List</h1>
-        <div className='container'>
-          <div className='to-do'>
-            Add an Item ...
-            <br />          
-            <input className='inputField' onChange={this.changeUserInput} value={this.state.userInput} type='text' placeholder='type item here ...' />
-            <button className='btn add-btn' onClick={this.addToList} >Add Item</button>
-            <ul>
-              {
-                this.state.list.map((val, idx) =>{
-                  return(
-                    <>
-                      <li className='list' key={val.id}>{idx + 1}- {val.value}</li>
-                      <button className='btn dele-btn' onClick={this.deleteItem(val.id)} >X</button>
-                    </>
-                    );
-                  }
-                )
-              }
-            </ul>
-          </div>
-        </div>
-      </>
-    );
-  }
-}
-
-export default App;
-*/
