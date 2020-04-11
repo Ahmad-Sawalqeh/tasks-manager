51e2c6c2db66f04e37d37c756385229fb0a1dc13
Ahmad-Sawalqeh <53290785+Ahmad-Sawalqeh@users.noreply.github.com>
2020-04-12 02:48:21 +0300

    adding bootstrap and font-awesome icons

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { v4 as uuid } from 'uuid';
+import { v4 as randomId } from 'uuid';
 import './app.css';
 
 function App(){
 
   const [userInput, setUserInput] = useState('');
   const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingItemId, setEditingItemId] = useState(0);
 
   function changeUserInput(e){
     let input = e.target.value
@@ -13,14 +15,29 @@ function App(){
   }
 
   function addToList(){
-    const newItem = {
-      id: uuid(),
-      value: userInput
+    if(userInput !== ''){
+      if(isEditing && editingItemId !== 0){
+        let listCopy = [...list];
+        let newList = listCopy.map(val => {
+          if(val.id === editingItemId) val.value = userInput;
+          return val;
+        });
+        setList(newList);
+        setUserInput('');
+        setIsEditing(!isEditing);
+        setEditingItemId(0);
+      }else{
+        const newItem = {
+          id: randomId(),
+          completed : false,
+          value: userInput
+        }
+        let newlist = [...list];
+        newlist.push(newItem);
+        setList(newlist);
+        setUserInput('');
+      }
     }
-    let newlist = [...list];
-    newlist.push(newItem);
-    setList(newlist);
-    setUserInput('');
   }
 
   function deleteItem(id){
@@ -29,29 +46,60 @@ function App(){
     setList(updateList);
   }
 
+  function itemToEdit(id){
+    const listCopy = [...list];
+    const editingItem = listCopy.filter(val => val.id === id);
+    let itemvalue = editingItem[0].value;
+    let itemId = editingItem[0].id;
+    setUserInput(itemvalue);
+    setIsEditing(!isEditing);
+    setEditingItemId(itemId);
+  }
+
+  function completed(id){
+    const listCopy = [...list];
+    let newList = listCopy.map(val => {
+      if(val.id === id) {
+        if(val.completed === false){
+          val.completed = true;
+        }else{
+          val.completed = false;
+        }
+      }
+      return val;
+    });
+    setList(newList);
+  }
+
   return (
     <>
-      <h1 className='app-title'>My List</h1>
-      <div className='container'>
-        <div className='to-do'>
-          Add an Item ...
-          <br />          
-          <input className='inputField' onChange={changeUserInput} value={userInput} type='text' placeholder='type item here ...' />
-          <button className='btn add-btn' onClick={addToList} >Add Item</button>
-          <ul>
-            {
-              list.map((val, idx) =>{
-                return(
-                  <>
-                    <li className='list' key={val.id}>{idx + 1}- {val.value}</li>
-                    <button className='btn dele-btn' onClick={()=>deleteItem(val.id)} >X</button>
-                  </>
-                  );
-                }
-              )
-            }
-          </ul>
-        </div>
+      <div className='container mt-5'>
+        <div className='card'>
+          <h1 className='ml-3 mt-3 font-weight-bold'>My List</h1>
+          <div className='card-body'>
+            <h3>Add an Item ...</h3>
+            <input className='form-control' onChange={changeUserInput} value={userInput} type='text' placeholder='type item here ...' />
+            <button className='btn btn-primary mt-2' onClick={addToList} >Add Item</button>          
+          </div>
+          <ul className='list-group'>
+              {
+                list.map((val, idx) =>{
+                  return(
+                    <>
+                      <div key={val.id}>
+                        <li key={val.id} className={`m-4 list-group-item ${val.completed ? 'completed' : ''}`} onClick={() => completed(val.id)} >
+                          {idx + 1}- {val.value}
+                        </li>
+                        <button className='btn btn-danger' onClick={()=>deleteItem(val.id)} >Delete <i className="fas fa-trash"></i></button>
+                        <button className='btn btn-success ml-3' onClick={()=>itemToEdit(val.id)} >Edit <i className="far fa-edit"></i></button>
+                      </div>
+                    </>
+                    );
+                  }
+                )
+              }
+            </ul>
+          </div>
       </div>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/js/all.min.js';
 import App from './app.js';
 
 function Root(){
