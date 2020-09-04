7ef9e8e855bef0115dfd41de6d2959d948b49939
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2020-09-04 19:50:19 +0300

    running build to deploy

---
+++ build/precache-manifest.4926c772008ed2888e24da449df68b4f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acd78dded66659ef09ee873bf9dd4258",
+    "url": "/tasks-manager/index.html"
+  },
+  {
+    "revision": "5c7382ae56e8bf359c18",
+    "url": "/tasks-manager/static/css/2.565a2d19.chunk.css"
+  },
+  {
+    "revision": "debd0b1fa8c28d9d8b37",
+    "url": "/tasks-manager/static/css/main.9ad3175d.chunk.css"
+  },
+  {
+    "revision": "5c7382ae56e8bf359c18",
+    "url": "/tasks-manager/static/js/2.4a6a7bee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tasks-manager/static/js/2.4a6a7bee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "debd0b1fa8c28d9d8b37",
+    "url": "/tasks-manager/static/js/main.d0d8ced7.chunk.js"
+  },
+  {
+    "revision": "686d821d4aba30a4dfd5",
+    "url": "/tasks-manager/static/js/runtime-main.fe2c146c.js"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/tasks-manager/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/tasks-manager/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/tasks-manager/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/tasks-manager/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/tasks-manager/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/tasks-manager/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/tasks-manager/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/tasks-manager/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/tasks-manager/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/tasks-manager/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/tasks-manager/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/tasks-manager/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/tasks-manager/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/tasks-manager/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/tasks-manager/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
