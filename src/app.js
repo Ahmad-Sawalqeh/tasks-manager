b903da51090467198b0e6ff6985af448b074e9f1
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2020-09-06 20:10:51 +0300

    creating statistic component

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from "react-redux";
-import { setList, setWantedListToShow } from './actions/actionCreater';
 import TasksStatistics from './components/tasksStatistics';
 import TasksBoard from './components/tasksBoard';
 import './app.css';
 
 /*
-** Task Manager MVC
+** Tasks Manager MVC
   1. add item
   2. delete item
   3. edit item
@@ -18,83 +16,7 @@ import './app.css';
   9. show only completed/In-Progress/canceled/Not-Started/all items
 */
 
-const initialList = {
-  "tasks": [
-       {
-          "id": 8688789987070970897,
-          "value": "create sitemap sketch, wireframe and mock-up.",
-          "currentStatus": {
-              "number": 3,
-              "state" : "completed"
-          },
-          "isEditing": false,
-      },
-      {
-          "id": 86887899875454545457,
-          "value": "select technoogies stack (programming languages, frameworks and CMS).",
-          "currentStatus": {
-              "number": 3,
-              "state" : "completed"
-          },
-          "isEditing": false,
-      },
-      {
-          "id": 65765686575454545457,
-          "value": "create colorfull page layouts and gets client's feedback also change the layout if required, test and upload the website to server.",
-          "currentStatus": {
-              "number": 1,
-              "state" : "In-Progress"
-          },
-          "isEditing": false,
-      },
-      {
-          "id": 657656478788487645457,
-          "value": "take 1 hour break before starting of building the initial website interface.",
-          "currentStatus": {
-              "number": 2,
-              "state" : "canceled"
-          },
-          "isEditing": false,
-      },
-      {
-          "id": 657656478788445435457,
-          "value": "build website by adding special features and interactivity.",
-          "currentStatus": {
-              "number": 0,
-              "state" : "Not-Started"
-          },
-          "isEditing": false,
-      },
-      {
-          "id": 6576589789988445435457,
-          "value": "get content ready with the power of SEO.",
-          "currentStatus": {
-              "number": 1,
-              "state" : "In-Progress"
-          },
-          "isEditing": false,
-      },
-      {
-          "id": 6578889789988445435457,
-          "value": "fix bugs ASAP and keep it up to date.",
-          "currentStatus": {
-              "number": 0,
-              "state" : "Not-Started"
-          },
-          "isEditing": false,
-      }
-  ]
-}
-
 function App(){
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(setList(initialList.tasks))
-    dispatch(setWantedListToShow(initialList.tasks));
-  }, [])
-
   return (
     <>
       <div className='container bg-light mt-3 rounded'>
--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -6,15 +6,83 @@ import {
     SET_COUNTER
   } from "../actions/actionTypes.js";
 
+  const initialList = {
+    "tasks": [
+         {
+            "id": 8688789987070970897,
+            "value": "create sitemap sketch, wireframe and mock-up.",
+            "currentStatus": {
+                "number": 3,
+                "state" : "completed"
+            },
+            "isEditing": false,
+        },
+        {
+            "id": 86887899875454545457,
+            "value": "select technoogies stack (programming languages, frameworks and CMS).",
+            "currentStatus": {
+                "number": 3,
+                "state" : "completed"
+            },
+            "isEditing": false,
+        },
+        {
+            "id": 65765686575454545457,
+            "value": "create colorfull page layouts and gets client's feedback also change the layout if required, test and upload the website to server.",
+            "currentStatus": {
+                "number": 1,
+                "state" : "In-Progress"
+            },
+            "isEditing": false,
+        },
+        {
+            "id": 657656478788487645457,
+            "value": "take 1 hour break before starting of building the initial website interface.",
+            "currentStatus": {
+                "number": 2,
+                "state" : "canceled"
+            },
+            "isEditing": false,
+        },
+        {
+            "id": 657656478788445435457,
+            "value": "build website by adding special features and interactivity.",
+            "currentStatus": {
+                "number": 0,
+                "state" : "Not-Started"
+            },
+            "isEditing": false,
+        },
+        {
+            "id": 6576589789988445435457,
+            "value": "get content ready with the power of SEO.",
+            "currentStatus": {
+                "number": 1,
+                "state" : "In-Progress"
+            },
+            "isEditing": false,
+        },
+        {
+            "id": 6578889789988445435457,
+            "value": "fix bugs ASAP and keep it up to date.",
+            "currentStatus": {
+                "number": 0,
+                "state" : "Not-Started"
+            },
+            "isEditing": false,
+        }
+    ]
+}
+
 const initialState = {
     userInput: '',
-    list: [],
+    list: initialList.tasks,
+    wantedListToShow: initialList.tasks,
     status: ['Not-Started', 'In-Progress', 'canceled', 'completed'],
     edit : {
         userInput: '',
         itemId: 0,
     },
-    wantedListToShow: [],
     counter: { 
         deleted: 2,
         edited: 4,
